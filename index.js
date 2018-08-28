567c35fb3c73c44412bfb96b3dc7a1282f630067
Gauravdeep Gosal <gdeep141@gmail.com>
2018-08-28 10:09:49 +0100

    added root directory to express path

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '.')));
 
 app.use(require('./routes'));
 
